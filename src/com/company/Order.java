04dd26adcd4aa777f651d48c493986e6f23fa8f9
wu <wu.yuyuan@hotmail.com>
2019-11-12 13:28:10 +0100

    story2

---
+++ src/com/company/Order.java
@@ -0,0 +1,31 @@
+package com.company;
+
+public class Order {
+/*
+    Story #2 : Menu de commandes
+    En tant qu'étudiant de la Coding Factory,
+    Je veux créer un menu en ligne de commande Afin de pouvoir contrôler mon application
+    Critères d'acceptation :
+    Création d'une classe avec des méthodes statiques pour la gestion de commandes
+    Ajout d'une commande d'aide (qui affiche les commandes possibles)
+    Ajout d'une commande pour quitter l'application
+    Affichage de la liste des commandes si la commande tapée est invalide
+    Tous les éléments du code et les commentaires sont en anglais Documentation du projet dans le code (JavaDoc)
+    Réalisation d'un diagramme de classe de l'application
+    Archivage sous Git du livrable de la story (code + diagramme de classe)
+        */
+
+    public static help {
+        System.out.println();
+
+    }
+    public static exit {
+
+
+    }
+    public static invalid{
+        
+    }
+
+
+}
